74c8181d1bc474f8b17e1fae84f033fdc2c14fc0
EtherSol <kylemz@email.arizona.edu>
2022-07-08 01:00:46 -0400

    =>

--- RuntimeJS/app.js
+++ RuntimeJS/app.js
@@ -1,13 +1,30 @@
 // function
-const EventEmmiter = require('events');
-const emitter = new EventEmmiter();
+// const EventEmmiter = require('events');
+// const emitter = new EventEmmiter();
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
 
-emitter.on('messagedLogged', function (eventArg) {
+// emitter.on('messagedLogged', function (eventArg) {
+//     console.log('Listener called', eventArg);
+// });
+
+emitter.on('messagedLogged', eventArg => {
     console.log('Listener called', eventArg);
 });
 
+emitter.on('logging', eventArgs => {
+    console.log("Listener Called", eventArgs)
+})
+
+// Raise an event
 emitter.emit('messagedLogged', { id: 1, url: 'http://' });
+
+// Raise: logging (data: message)
+emitter.emit('logging', { id_data: 1, message: "Hello" })
+
+
 // console.log(async_files)
 
 // // // Node Global objects
